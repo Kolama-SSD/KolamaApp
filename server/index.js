7fce2cdaa4d733b789c2e5c57443764e31976302
IT20230692 <IT20230692@my.sliit.lk>
2023-10-03 20:23:21 +0530

    add cors protection stop the invade responses access

--- server/index.js
+++ server/index.js
@@ -28,10 +28,26 @@ mongoose
   })
   .catch((err) => console.log(`DB connection error`, err));
 
-  app.use('/api/auth', authRoute);
-  app.use('/api/users', userRoute);
-  app.use('/api/adds', addRoute);
-  app.use('/api/addskolam', addKolamRoute);
-  app.use('/api/addsmask', addMaskRoute);
-  
-  export const handler = serverless(app);
+// Define CORS configuration with allowed origins.
+const corsOptions = {
+  // Specify the origins that are allowed to access your API.
+  origin: ['https://kolamasrilanka.netlify.app/'],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+
+app.use('/api/auth', cors(corsOptions), authRoute);
+app.use('/api/users', cors(corsOptions), userRoute);
+
+// Define custom CORS settings for the '/api/adds' route.
+app.use('/api/adds', cors({ origin: 'https://kolamasrilanka.netlify.app/' }), addRoute);
+
+// Define custom CORS settings for the '/api/addskolam' route.
+app.use('/api/addskolam', cors({ origin: 'https://kolamasrilanka.netlify.app/' }), addKolamRoute);
+
+// Define custom CORS settings for the '/api/addsmask' route.
+app.use('/api/addsmask', cors({ origin: 'https://kolamasrilanka.netlify.app/' }), addMaskRoute);
+
+export const handler = serverless(app);
