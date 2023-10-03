f188e25a9af2ed5a0b502a37a0525878ab4c75d4
IT20230692 <IT20230692@my.sliit.lk>
2023-10-03 20:22:23 +0530

    add env for security and store mongoDB url there

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
+
+
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import cors from 'cors';
 import addRoute from './routes/add.route.js';
 import addKolamRoute from './routes/addKolam.route.js';
@@ -10,13 +13,13 @@ import userRoute from './routes/user.route.js';
 import serverless from 'serverless-http';
 
 const app = express();
-
-app.use(cors());
+dotenv.config();
 app.use(express.json());
 
 mongoose.set('strictQuery', false);
+
 mongoose
-  .connect('mongodb+srv://Kolama:Kolama@kolama.e0d7ieh.mongodb.net/Kolama', {
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -25,10 +28,10 @@ mongoose
   })
   .catch((err) => console.log(`DB connection error`, err));
 
-app.use('/api/auth', authRoute);
-app.use('/api/users', userRoute);
-app.use('/api/adds', addRoute);
-app.use('/api/addskolam', addKolamRoute);
-app.use('/api/addsmask', addMaskRoute);
-
-export const handler = serverless(app);
+  app.use('/api/auth', authRoute);
+  app.use('/api/users', userRoute);
+  app.use('/api/adds', addRoute);
+  app.use('/api/addskolam', addKolamRoute);
+  app.use('/api/addsmask', addMaskRoute);
+  
+  export const handler = serverless(app);
